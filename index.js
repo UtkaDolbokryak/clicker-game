f8206975cc68defefd550593a8b936bb1e47a7a4
Голиков Ярослав <y.golikov@kvantorium62.local>
2020-10-29 20:00:15 +0300

    add interval upgrade

--- index.js
+++ index.js
@@ -1,17 +1,54 @@
 let points = 0;
-let pointsPerClick = 1;
-function keyPresskHandler(event) {
+let pointsPerTick = 1;
+let upgradePrice = 50;
+let intervalUpgradePrice = 50;
+let timeInterval = 1000;
+let upgradeBtn = document.getElementById("upgrade-ppt-btn");
+let upgradeIntervalBtn = document.getElementById("upgrade-time-interval-btn");
+
+function keyPressHandler(event) {
   if (event.code === "Space") {
-    //points = points + pointsPerClick;
-    points += pointsPerClick;
-    document.getElementById("points").innerText = points;
+    increasePoints();
   }
 }
 
 function increasePoints() {
-  points += pointsPerClick;
+  points += pointsPerTick;
   document.getElementById("points").innerText = points;
 }
 
-document.addEventListener("keypress", keyPresskHandler);
-setInterval(increasePoints, 1000);
+function upgradePointsPerTick() {
+  if (points >= upgradePrice) {
+    points -= upgradePrice;
+    upgradePrice *= 2;
+    ++pointsPerTick;
+    let pps = (1000 * pointsPerTick) / timeInterval;
+    pps = Math.round(pps * 100) / 100;
+    document.getElementById("upgrade-ppt-price").innerText = upgradePrice;
+    document.getElementById("points-per-second").innerText = pps;
+    document.getElementById("points").innerText = points;
+  }
+}
+
+function upgradeTimeInterval() {
+  if (points >= intervalUpgradePrice) {
+    points -= intervalUpgradePrice;
+    intervalUpgradePrice *= 2;
+    timeInterval *= 0.8;
+    let pps = (1000 * pointsPerTick) / timeInterval;
+    pps = Math.round(pps * 100) / 100;
+    document.getElementById(
+      "upgrade-time-interval-price"
+    ).innerText = intervalUpgradePrice;
+    document.getElementById("points-per-second").innerText = pps;
+    document.getElementById("points").innerText = points;
+
+    clearInterval(interval);
+    interval = setInterval(increasePoints, timeInterval);
+  }
+}
+
+document.onkeypress = keyPressHandler;
+upgradeBtn.onclick = upgradePointsPerTick;
+upgradeIntervalBtn.onclick = upgradeTimeInterval;
+let interval = setInterval(increasePoints, timeInterval);
